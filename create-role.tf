423f04bf55d8fecbc4067b82f54bd889956e272b
Fabio Montezuma <fabio.montezuma@soaexpert.com.br>
2019-08-06 16:35:22 -0300

    Initial commit

---
+++ create-role.tf
@@ -0,0 +1,50 @@
+resource "aws_iam_role" "devops" {
+  name = "devops"
+
+  assume_role_policy = <<EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        },
+        {
+          "Effect": "Allow",
+          "Principal": {
+            "AWS": "arn:aws:iam::${var.codebuild_deploy_role_arn}:root"
+          },
+          "Action": "sts:AssumeRole",
+          "Condition": {}
+        }
+      ]
+    }
+EOF
+}
+
+resource "aws_iam_policy" "devops" {
+  name        = "devops-policy"
+  description = "A devops policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "devops-attach" {
+  role       = "${aws_iam_role.devops.name}"
+  policy_arn = "${aws_iam_policy.devops.arn}"
+}
---
+++ variables.tf
@@ -0,0 +1,1 @@
+variable devops_account {}
