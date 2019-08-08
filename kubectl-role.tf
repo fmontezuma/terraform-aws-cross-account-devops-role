b268e5c3c25f5e0d4035847e0b326bc0ccd91e12
Fabio Montezuma <fabio.montezuma@soaexpert.com.br>
2019-08-08 14:14:38 -0300

    Add kubectl role for cross account access to Kubernetes

---
+++ kubectl-role.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role" "kubectl" {
+  name = "kubectl"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "sts:AssumeRole"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${var.devops_account}:root"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {}
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "kubectl-policy" {
+  name = "eks-access"
+  role = "${aws_iam_role.kubectl.name}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "eks:Describe*",
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
